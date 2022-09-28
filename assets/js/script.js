b09ee105eb62a2aa7a837a97d5c4085049f08dca
TreyKockelman <kockelmant@gmail.com>
2022-09-27 22:02:26 -0500

    added local storage element for searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,17 @@
-const weatherDays = [];
 let currDay = moment.now();
+var currCard = $('.curr-weather');
 var cardContainer = $('.card-container');
+var citiesDisplay = JSON.parse(localStorage.getItem("searches")) || [];
 
-
-
-// sampleData.list.forEach( function(tsObj){
-
-//   // Makes a moment date object for each record
-//   const dateObj = moment.unix(tsObj.dt)
-
-//   // Generate the day # for the day in the date object
-//   const dateNum = dateObj.format("DDD")
-
-//   // If the current date in tsObj hasn't had a record put into weatherDays, do that now 
-//   // Then skip over all other records for this day
-//   if( dateNum !== currDay && weatherDays.length < 5 ){
-//     weatherDays.push( tsObj )
-//     currDay = dateNum
-//   }
-
-// })
+function appendElements(date, temp, wind, hum) {
+  var parentEl = $("<card>");
+  var cardsDate = $("<h3>").text(moment(date).format("L") + " ");
+  var cardsTemp = $("<p>").text("Temp: " + temp + "\u00B0F ");
+  var cardsWind = $("<p>").text("Wind: " + wind + " ");
+  var cardsHum = $("<p>").text("Humidity: " + hum + " ");
+  parentEl.append(cardsDate, cardsTemp, cardsWind, cardsHum);
+  $('.card-container').append(parentEl);
+}
 
 function getAPI(city) {
   fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=b6d9290b58c34b6ad6a2f5f35449e3ec')
@@ -28,19 +20,42 @@ function getAPI(city) {
   })
   .then(function (data) {
     for (var i = 3; i < data.list.length; i+=8) {
-    console.log(moment().format('MMM Do YY'));
-    console.log(data);
-    console.log(data.list[i].dt_txt);
-    console.log(data.list[i].main.temp);
-    console.log(data.list[i].wind.speed);
-    console.log(data.list[i].main.humidity);
+      var date = data.list[i].dt_txt
+      var temperature = data.list[i].main.temp
+      var wind = data.list[i].wind.speed
+      var humidity = data.list[i].main.humidity
+      appendElements(date, temperature, wind, humidity);
     }
   });
+}
+
+function getCurrentWeather(city) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=b6d9290b58c34b6ad6a2f5f35449e3ec')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    var date = moment().format("L")
+    var temperature = data.main.temp
+    var wind = data.wind.speed
+    var humidity = data.main.humidity
+    appendElements(date, temperature, wind, humidity);
+  });
+}
+
+function saveSearch() {
+  var citiesEntered = $('#city-input').val();
+  citiesDisplay.push(citiesEntered);
+  localStorage.setItem("searches", JSON.stringify(citiesDisplay));
+
 }
 
 $('#submitBtn').on('click', function(event) {
   event.preventDefault()
   var searchInput = $('#city-input').val();
-  // console.log(searchInput);
+  $('.card-container').empty();
   getAPI(searchInput);
+  getCurrentWeather(searchInput);
+  saveSearch();
 });
